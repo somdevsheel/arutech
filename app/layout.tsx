446848f5f6b35b7cd44481e09f694c7fcf71c503
Somdev Sheel <somdevsheel7@gmail.com>
2026-07-20 14:45:32 +0530

    add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -185,6 +185,10 @@ export default function RootLayout({
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
         <script dangerouslySetInnerHTML={{ __html: gaScript }} />
 
+        {/* Meta Pixel */}
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1555074419732103');fbq('track','PageView');` }} />
+        <noscript><img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1555074419732103&ev=PageView&noscript=1" alt="" /></noscript>
+
         {/* JSON-LD Schema */}
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: schemaOrg }} />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: schemaWebsite }} />
